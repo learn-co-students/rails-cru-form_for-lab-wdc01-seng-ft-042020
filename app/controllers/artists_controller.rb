dabee1acf4700dcc8f8487178e5c76cdbad90ffc
agdsarge <agdsargeant@gmail.com>
2020-05-18 16:08:47 -0400

    destroy added for songs

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,19 +1,19 @@
 class ArtistsController < ApplicationController
-    
-    
+
+
     def index
         @artists = Artist.all
     end
 
     def show
         @artist = Artist.find(params[:id])
-       
+
     end
-        
+
     def edit
      @artist = Artist.find(params[:id])
     end
-    
+
     def new
         @artist = Artist.new
     end
@@ -38,7 +38,7 @@ class ArtistsController < ApplicationController
         redirect_to artists_path
     end
 
-   
 
 
-    end
+
+end
